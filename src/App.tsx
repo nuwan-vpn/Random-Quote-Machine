b23c439e80e63c2bab6dca390d71e09f3a25bc5b
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-04-19 15:51:28 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [quote, setQuote] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
