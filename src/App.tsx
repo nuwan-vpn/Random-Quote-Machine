257572b0947fdda78234734944de080199c8e108
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-04-19 23:43:03 +0530

    refactor: Refactor code for accessing quotes data

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import quotesData from './assets/quotes.json';
+
+interface Quote {
+  quote: string;
+  author: string;
+}
 
 function App() {
-  interface Quote{
-    quote:string,
-    author:string
-  }
+  
+  const { quotes } = quotesData;
 
-  const getRandomQuote = () : Quote{
-    
-  }
+  const getRandomQuote = (): Quote => {
+    return quotes[Math.floor(Math.random() * quotes.length)];
+  };
 
   const [quote, setQuote] = useState<Quote>(getRandomQuote());
 
   return (
     <>
-      
+      {/* Render the quote and author */}
+      <div>
+        <p>{quote.quote}</p>
+        <p>- {quote.author}</p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
