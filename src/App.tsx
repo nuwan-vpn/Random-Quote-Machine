455e1c724f358f17cd48257fa977dfe19d0d420d
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-04-20 22:25:35 +0530

    feat: Integrate react-icons and add quote box styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { FaTwitter, FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
 import './App.css';
 import quotesData from './assets/quotes.json';
 
@@ -22,7 +23,9 @@ function App() {
       {/* Render the quote and author */}
       <div id="quote-box">
         <div className="quote-content">
+          <FaQuoteLeft size="20" style={{marginRight:"10px"}}/>
           <h2 id="text">{quote.quote}</h2>
+          <FaQuoteRight size="20" style={{marginRight:"10px"}}/>
           <h4 id="author">- {quote.author}</h4>
         </div>
       </div>
